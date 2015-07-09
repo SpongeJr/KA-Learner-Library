e1ea797b458b3ce5fca1b2fd464315f3f1b5f60e
SpongeJr <spongejr@gmail.com>
2015-07-09 02:36:44 -0400

    Cleanup from last commit.
    Fixed whitespace issues; removed debugging console.log()s (whoops!)

--- KA_LL.js
+++ KA_LL.js
@@ -8,62 +8,61 @@ var KA_LL = {
 	},
 	onKA: (document.location.origin === "https://www.kasandbox.org"),
 	storage: {
-	    // A function that stores data in sessionStorage
-	    // if program is running in KA sandbox, uses localStorage
-	    // otherwise.
-	    // May be updated with techniques that mimic localStorage on KA
-	    // allowing programs extra functionality without modification.
+		// A function that stores data in sessionStorage
+		// if program is running in KA sandbox, uses localStorage
+		// otherwise.
+		// May be updated with techniques that mimic localStorage on KA
+		// allowing programs extra functionality without modification.
 		get: function(key, value) {
-            if (KA_LL.onKA) {
-                return sessionStorage.getItem(key, value);
-            } else {
-                return localStorage.getItem(key, value);
-            }},
+			if (KA_LL.onKA) {
+				return sessionStorage.getItem(key, value);
+			} else {
+				return localStorage.getItem(key, value);
+			}},
 		set: function(key, value) {
-            if (KA_LL.onKA) {
-                sessionStorage.setItem(key, value);
-            } else {
-                localStorage.setItem(key, value);
-            }},
+			if (KA_LL.onKA) {
+				sessionStorage.setItem(key, value);
+			} else {
+				localStorage.setItem(key, value);
+			}},
 		clear: function() {
-		    if (KA_LL.onKA) {
-                sessionStorage.clear();
-            } else {
-                localStorage.clear();
-            }},
+			if (KA_LL.onKA) {
+				sessionStorage.clear();
+			} else {
+				localStorage.clear();
+			}},
 		remove: function() {
-            if (KA_LL.onKA) {
-                sessionStorage.removeItem(key, value);
-            } else {
-                localStorage.removeItem(key, value);
-            }
+			if (KA_LL.onKA) {
+				sessionStorage.removeItem(key, value);
+			} else {
+				localStorage.removeItem(key, value);
+			}
 		}},
 	randomChoice: function(choices) {
-	    /**
-        * Ever wanted to choose randomly between several options?
-        * Tried doing so with the random(min, max) function?
-        * That didn't work out for you, did it?
-        * That's because random(min, max) only works with numbers and
-        * gives ANYTHING in between. Even fractions.
-        * 
-        * Well! Now there's the randomChoice(choices) function!
-        * @author Dalendrion
-        * @param {...Mixed} choices - Any number of any type of value you could ever want!       * @returns {Mixed} Returns one of the given arguments at random.
-        *                  each argument has an equal chance of being returned.
-        */
-        return arguments[floor(random(arguments.length))];
-        },
+		/**
+		* Ever wanted to choose randomly between several options?
+		* Tried doing so with the random(min, max) function?
+		* That didn't work out for you, did it?
+		* That's because random(min, max) only works with numbers and
+		* gives ANYTHING in between. Even fractions.
+		* 
+		* Well! Now there's the randomChoice(choices) function!
+		* @author Dalendrion
+		* @param {...Mixed} choices - Any number of any type of value you could ever want!
+		* @returns {Mixed} Returns one of the given arguments at random.
+		*			each argument has an equal chance of being returned.
+		*/
+		return arguments[floor(random(arguments.length))];
+	},
 	randomizeChildren: function(tagName) {
-	    // randomizes the children of element type provided
-	    // snagged from @pamela's contest result programs, modified
-	    var lists = document.getElementsByTagName(tagName);
-	    console.log(lists);
-	    for (var j = 0; j < lists.length; j++) {
-	        var theList = lists[j];
-	        console.log(theList);
-	        for (var i = theList.children.length; i >= 0; i--) {
-               theList.appendChild(theList.children[Math.random() * i | 0]);
-            }
-	    }
-    }
+		// randomizes the children of element type provided
+		// snagged from @pamela's contest result programs, modified
+		var lists = document.getElementsByTagName(tagName);
+		for (var j = 0; j < lists.length; j++) {
+			var theList = lists[j];
+			for (var i = theList.children.length; i >= 0; i--) {
+				theList.appendChild(theList.children[Math.random() * i | 0]);
+			}
+		}
+	}
 };
